681402953c739d9f2fac6a06193118c048bc0c4e
Ryan Clements <ryanclementshax@gmail.com>
2021-09-10 11:52:11 -0400

    add theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './Global'
 import Header from './layouts/Header'
 import { Button, Card } from './elements'
 
+const theme = {
+  colors: {
+    primary: '#E54B4B',
+    secondary: '#DBDE61'
+  }
+}
+
+const theme2 = {
+  colors: {
+    primary: '#524763',
+    secondary: '#82D8D8'
+  }
+}
+
 function App() {
   return (
-    <div>
-      <Header />
-      <GlobalStyle />
-      <main>
-        <Card>
-          <h2>Hello from card</h2>
-          <Card.Button>Hello</Card.Button>
-        </Card>
-        <Button>Hello</Button>
-        <Button modifiers="cancel">Cancel</Button>
-        <Button modifiers="small">Hello</Button>
-        <Button modifiers={["small", "cancel"]}>Cancel</Button>
-      </main>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Header />
+        <GlobalStyle />
+        <main>
+          <ThemeProvider theme={theme2}>
+            <Card>
+              <h2>Hello from card</h2>
+              <Card.Button>Hello</Card.Button>
+            </Card>
+          </ThemeProvider>
+          <Button>Hello</Button>
+          <Button modifiers="cancel">Cancel</Button>
+          <Button modifiers="small">Hello</Button>
+          <Button modifiers={["small", "cancel"]}>Cancel</Button>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/elements/Buttons.js
+++ src/elements/Buttons.js
@@ -7,8 +7,9 @@ const BUTTON_MODIFIERS = {
     font-size: 1rem;
     padding: 3px 10px;
   `,
-  cancel: () => `
+  cancel: ({ theme }) => `
     background: tomato;
+    background: ${theme.colors.secondary};
   `
 }
 
@@ -19,7 +20,7 @@ export const Button = styled.button`
   font-size: 2rem;
   border: none;
   transition: 0.3s ease box-shadow;
-  background: ${teal};
+  background: ${props => props.theme.colors.primary};
   ${elevation[1]}
   &:hover {
     ${elevation[2]}
