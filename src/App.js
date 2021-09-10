2cb61b1fbfff4960b32e1ce879ce17839e2a9bb6
Ryan Clements <ryanclementshax@gmail.com>
2021-09-10 09:51:36 -0400

    use global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,104 @@
 import logo from './logo.svg';
-import './App.css';
+import styled, { css } from 'styled-components'
+import GlobalStyle from './Global'
+
+const size = {
+  small: 400,
+  med: 960,
+  large: 1140
+}
+
+// const above = Object.keys(size).reduce((acc, label) => {
+//   acc[label] = (...args) => css`
+//     @media(min-width: ${size[label]}px) {
+//       ${css(...args)}
+//     }
+//   `
+//   return acc
+// }, {})
+
+const above = Object.keys(size).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media(min-width: ${size[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+  return acc
+}, {})
+
+const below = Object.keys(size).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media(max-width: ${size[label] / 16}em) {
+      ${css(...args)}
+    }
+  `
+  return acc
+}, {})
+
+// CSS Helper
+const fixedTop = css`
+  position: fixed;
+  top: ${({ top }) => top + 'px'};
+  left: 0;
+`
+
+const Heading = styled.h1`
+  font-size: 2rem;
+  ${above.med`
+    color: blue;
+  `}
+`
+
+const color = "white";
+
+const Button = styled.button`
+  padding: 5px 20px;
+  border-radius: 4px;
+  border: none;
+  color: ${color};
+  font-size: 2rem;
+  background: indigo;
+`;
+
+const CancelButton = styled(Button)`
+  background: tomato;
+  ${fixedTop}
+`;
+
+const AppWrapper = styled.div`
+  header {
+    background: teal;
+  }
+  ${Button} {
+    margin-bottom: 2rem;
+  }
+`
+
+const Fake = ({ className }) => (
+  <div className={className}>
+    <h2>I'm a fake component</h2>
+  </div>
+)
+
+const DoubleFake = styled(Fake)`
+  h2 {
+    color: red;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
+    <AppWrapper>
+      <GlobalStyle />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Heading>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Heading>
+        <DoubleFake />
+        <Fake />
+        <Button>Save</Button>
+        <CancelButton top={100}>Cancel</CancelButton>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +108,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </AppWrapper>
   );
 }
 
