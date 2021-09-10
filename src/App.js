6bdedf6ff8aca7454b5c8dcefbac7c83cd42d22c
Ryan Clements <ryanclementshax@gmail.com>
2021-09-10 11:30:00 -0400

    refactor button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import GlobalStyle from './Global'
 import Header from './layouts/Header'
-
+import { Button, CancelButton } from './elements'
 
 function App() {
   return (
     <div>
       <Header />
       <GlobalStyle />
+      <Button>Hello</Button>
+      <CancelButton>Cancel</CancelButton>
+      <Button size="small">Hello</Button>
+      <CancelButton size="small">Cancel</CancelButton>
     </div>
   );
 }
--- src/elements/Buttons.js
+++ src/elements/Buttons.js
@@ -1,18 +1,28 @@
 import styled from 'styled-components'
-import { fixedTop } from '../utilities'
-
-const color = "white";
+import { teal, elevation } from '../utilities'
 
 export const Button = styled.button`
   padding: 5px 20px;
   border-radius: 4px;
-  border: none;
-  color: ${color};
+  color: white;
   font-size: 2rem;
-  background: indigo;
+  border: none;
+  transition: 0.3s ease box-shadow;
+  background: ${teal};
+  ${elevation[1]}
+  &:hover {
+    ${elevation[2]}
+  }
+  ${({ size }) => {
+    if(size === 'small') {
+      return `
+        font-size: 1rem;
+        padding: 3px 10px;
+      `
+    }
+  }}
 `;
 
 export const CancelButton = styled(Button)`
   background: tomato;
-  ${fixedTop}
 `;
